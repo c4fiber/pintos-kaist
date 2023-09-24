674e3ed360a8e4610e4a7559924f4514a845bbfb
Designated-Hitter <dosadola@gmail.com>
2023-09-24 20:49:20 +0900

    added gitignore

--- do.sh
+++ do.sh
@@ -6,6 +6,6 @@ source ../../activate
 # pintos -- -q run alarm-single
 # pintos -- -q run alarm-multiple
 # pintos -- -q run alarm-simultaneous
-# pintos -- -q run alarm-priority
+pintos -- -q run alarm-priority
 # pintos -- -q run alarm-zero
 # pintos -- -q run alarm-negative
